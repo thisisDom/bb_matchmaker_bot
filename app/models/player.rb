c5ff73903682d40314d28479351191fdfd1b0021
thisisDom <Dominique.Crowther@gmail.com>
2017-07-18 07:19:49 -0700

    adds Player model

---
+++ app/models/player.rb
@@ -0,0 +1,10 @@
+class Player < ApplicationRecord
+
+  validates :username, :discord_id, { presence: true,
+                                      uniqueness: { case_sensetive: false }
+                                    }
+  validates :discord_id, :elo { numericality: { only_integer: true,
+                                                greater_than_or_equal_to: 0
+                                              }
+                              }
+end
