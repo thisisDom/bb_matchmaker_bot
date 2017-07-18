b2a89c64ab2c833c15e35c9d19bc8faa8ca650e2
thisisDom <Dominique.Crowther@gmail.com>
2017-07-18 07:35:20 -0700

    refactors validations into own describe block and fixes errors

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,72 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
-  let(:player) { Player.new }
 
   describe 'Attributes' do
-    describe 'username' do
-      context 'reading/writing' do
-        it 'can set and get its username' do
-          player.username = 'Test'
-          expect(player.username).to eq 'Test'
-        end
+    let(:player) { Player.new }
+
+    it 'can set and get its username' do
+      player.username = 'Test'
+      expect(player.username).to eq 'Test'
+    end
+
+    it 'can set and get its discord_id' do
+      player.discord_id = 1080
+      expect(player.discord_id).to eq 1080
+    end
+
+    it 'can set and get its discord_id' do
+      player.elo = 1500
+      expect(player.elo).to eq 1500
+    end
+  end
+
+  describe 'Validations' do
+    context 'username' do
+      it 'is present' do
+        player = Player.create(username: nil, discord_id: 1200, elo: 1000)
+        expect(player.errors.full_messages.length).to eq 1
       end
 
-      context 'validations' do
-        it 'is present' do
-          player = Player.create(username: nil, discord_id: nil, elo: 1000)
-          expect(player.errors.full_messages.length).to eq 1
-        end
-
-        it 'is unique' do
-          Player.create(username: 'Test', discord_id: nil, elo: 1000)
-          player = Player.create(username: 'Test', discord_id: nil, elo: 1000)
-          expect(player.errors.full_messages.length).to eq 1
-        end
+      it 'is unique' do
+        Player.create(username: 'Test', discord_id: 1200, elo: 1000)
+        player = Player.create(username: 'Test', discord_id: 1300, elo: 1000)
+        expect(player.errors.full_messages.length).to eq 1
       end
     end
 
-    describe 'discord_id' do
-      context 'reading/writing' do
-        player.discord_id = 1080
-        expect(discord_id).to eq 1080
+    context 'discord_id' do
+      it 'is present' do
+        player = Player.create(username: 'Test', discord_id: nil, elo: 1000)
+        expect(player.errors.full_messages.length).to eq 2
       end
 
-      context 'validations' do
-        it 'is present' do
-          player = Player.create(username: 'Test', discord_id: nil, elo: 1000)
-          expect(player.errors.full_messages.length).to eq 1
-        end
+      it 'is an integer and greater than 0' do
+        player = Player.create(username: 'Test', discord_id: 0.45, elo: 1000)
+        expect(player.errors.full_messages.length).to eq 1
 
-        it 'is an integer' do
-          player = Player.create(username: 'Test', discord_id: .45, elo: 1000)
-          expect(player.errors.full_messages.length).to eq 1
-        end
+        player = Player.create(username: 'Test', discord_id: -1, elo: 1000)
+        expect(player.errors.full_messages.length).to eq 1
+      end
 
-        it 'is unique' do
-          Player.create(username: 'Test', discord_id: 1080, elo: 1050)
-          player = Player.create(username: 'Test', discord_id: 1080, elo: 1000)
-          expect(player.errors.full_messages.length).to eq 1
-        end
+      it 'is unique' do
+        Player.create(username: 'Test', discord_id: 1080, elo: 1050)
+        player = Player.create(username: 'Test1', discord_id: 1080, elo: 1000)
+        expect(player.errors.full_messages.length).to eq 1
       end
     end
 
-    describe 'elo' do
-      context 'reading/writing' do
-        player.elo = 1500
-        expect(elo).to eq 1500
-      end
+    context 'elo' do
+      it 'is an integer and greater than 0' do
+        player = Player.create(username: 'Test', discord_id: 1080, elo: 0.45)
+        expect(player.errors.full_messages.length).to eq 1
 
-      context 'validations' do
-        it 'is an integer and greater than 0' do
-          player = Player.create(username: 'Test', discord_id: 1080, elo: .45)
-          expect(player.errors.full_messages.length).to eq 1
-
-          player = Player.create(username: 'Test', discord_id: 1080, elo: -1)
-          expect(player.errors.full_messages.length).to eq 1
-        end
+        player = Player.create(username: 'Test', discord_id: 1080, elo: -1)
+        expect(player.errors.full_messages.length).to eq 1
       end
     end
-
   end
 end
