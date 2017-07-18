083f8b6e466d3d9c5112ffada5b85a63a11fb2c1
thisisDom <Dominique.Crowther@gmail.com>
2017-07-18 08:08:17 -0700

    fixes typo

--- spec/models/queued_player_spec.rb
+++ spec/models/queued_player_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe QueuedPlayer, type: :model do
   describe 'Associations' do
-    context 'player' do
+    context 'Player' do
       it 'belongs to a player' do
         player_id = Player.create(username: 'Test', discord_id: 1200, elo: 1000).id
         queued_player = QueuedPlayer.create(player_id: player_id)
