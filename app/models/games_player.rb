9e47af66cfbd8d692d325ed2eb15d07dcfb8d272
thisisDom <Dominique.Crowther@gmail.com>
2017-07-18 15:52:18 -0700

    adds GamesPlayer model

---
+++ app/models/games_player.rb
@@ -0,0 +1,10 @@
+class GamesPlayer < ApplicationRecord
+  belongs_to :player
+  belongs_to :game
+
+  validates :team, :pregame_elo, :expected_win_change, :expected_loss_change, { numericality: { only_integer: true,
+                                                                                                greater_than_or_equal_to: 0
+                                                                                              }
+                                                                              }
+  
+end
