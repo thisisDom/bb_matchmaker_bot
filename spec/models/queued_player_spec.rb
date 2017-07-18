30cba6cc59300379aaea708b7e2608576449f829
thisisDom <Dominique.Crowther@gmail.com>
2017-07-18 08:04:16 -0700

    fixes errors

--- spec/models/queued_player_spec.rb
+++ spec/models/queued_player_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.describe QueuedPlayer, type: :model do
   describe 'Associations' do
     context 'player' do
-      player_id = Player.create(username: 'Test', discord_id: 0.45, elo: 1000).id
-      queued_player = QueuedPlayer.create(player_id: player_id)
-      expect(queued_player.player).to be_a(Player)
+      it 'belongs to a player' do
+        player_id = Player.create(username: 'Test', discord_id: 1200, elo: 1000).id
+        queued_player = QueuedPlayer.create(player_id: player_id)
+        expect(queued_player.player).to be_a(Player)
+      end
     end
   end
 end
