3e5e560b3f030c29314b4fb417d5b9d7ae02928a
thisisDom <Dominique.Crowther@gmail.com>
2017-07-18 07:34:47 -0700

    fixes a typo error

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,7 +3,7 @@ class Player < ApplicationRecord
   validates :username, :discord_id, { presence: true,
                                       uniqueness: { case_sensetive: false }
                                     }
-  validates :discord_id, :elo { numericality: { only_integer: true,
+  validates :discord_id, :elo, { numericality: { only_integer: true,
                                                 greater_than_or_equal_to: 0
                                               }
                               }
