bf307e4233e0ff8e963d781f84c1d42f28a39140
thisisDom <Dominique.Crowther@gmail.com>
2017-07-18 09:38:06 -0700

    adds before and after hooks for creating and deleting a player

--- spec/models/queued_player_spec.rb
+++ spec/models/queued_player_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe QueuedPlayer, type: :model do
+  before(:all) do
+    Player.create(username: 'Test', discord_id: 1200)
+  end
+  after(:all) do
+    Player.destroy_all
+  end
+  
   describe 'Associations' do
     context 'Player' do
       it 'belongs to a player' do
-        player_id = Player.create(username: 'Test', discord_id: 1200, elo: 1000).id
-        queued_player = QueuedPlayer.create(player_id: player_id)
+        queued_player = QueuedPlayer.create(player_id: Player.first.id)
         expect(queued_player.player).to be_a(Player)
+        expect(queued_player.player).to eq(Player.first)
       end
     end
   end
