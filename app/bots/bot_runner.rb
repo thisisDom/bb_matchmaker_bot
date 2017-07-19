14624902575e029742c0d878cf9055adf7683df1
thisisDom <Dominique.Crowther@gmail.com>
2017-07-18 18:51:41 -0700

    adds a runner script to run the bot

---
+++ app/bots/bot_runner.rb
@@ -0,0 +1,15 @@
+require 'discordrb'
+require 'dotenv/load'
+require_relative 'matchmaking_bot_events'
+require_relative 'matchmaking_bot_commands'
+
+bot = Discordrb::Commands::CommandBot.new(token: ENV['DISCORDAPI'], client_id: ENV['DISCORDCLIENT'], prefix: '!')
+bot.set_user_permission(ENV['BOTOWNER'], 9001)
+
+
+puts "This bot's invite URL is #{bot.invite_url}."
+puts 'Click on it to invite it to your server.'
+
+bot.include! MatchmakingEvents
+bot.include! MatchmakingCommands
+bot.run
