ffe3d6fda0b056fddc5f392053d7e0eda8c8b6e8
thisisDom <Dominique.Crowther@gmail.com>
2017-07-18 21:25:26 -0700

    adds conversation of environment variable “botowner” id to a string

--- app/bots/bot_runner.rb
+++ app/bots/bot_runner.rb
@@ -4,7 +4,7 @@ require_relative 'matchmaking_bot_events'
 require_relative 'matchmaking_bot_commands'
 
 bot = Discordrb::Commands::CommandBot.new(token: ENV['DISCORDAPI'], client_id: ENV['DISCORDCLIENT'], prefix: '!')
-bot.set_user_permission(ENV['BOTOWNER'], 9001)
+bot.set_user_permission(ENV['BOTOWNER'].to_i, 9001)
 
 
 puts "This bot's invite URL is #{bot.invite_url}."
