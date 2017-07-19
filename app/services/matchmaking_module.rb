5b95e98570d7ab810b6712c74b32644129c2dfde
thisisDom <Dominique.Crowther@gmail.com>
2017-07-19 07:54:48 -0700

    refactors make_game method

--- app/services/matchmaking_module.rb
+++ app/services/matchmaking_module.rb
@@ -5,33 +5,8 @@ module Matchmaking
     team_1_expected_result = Elo.expected_result({team: team_1, opponent: team_2})
     team_2_expected_result = 1 - team_1_expected_result
 
-    team_1.map! { |player|
-      expected_change = Elo.expected_change(player.elo, team_1_expected_result)
-
-      details = {
-        team: 1,
-        player_id: player.id,
-        game_id: game.id,
-        pregame_elo: player.elo,
-        expected_loss_change: expected_change[:loss],
-        expected_win_change: expected_change[:win],
-      }
-      GamesPlayer.create(details)
-    }
-
-    team_2.map! { |player|
-      expected_change = Elo.expected_change(player.elo, team_1_expected_result)
-
-      details = {
-        team: 2,
-        player_id: player.id,
-        game_id: game.id,
-        pregame_elo: player.elo,
-        expected_loss_change: expected_change[:loss],
-        expected_win_change: expected_change[:win]
-      }
-      GamesPlayer.create(details)
-    }
+    team_1 = Matchmaking.make_games_players(game, team_1, 1, team_1_expected_result)
+    team_2 = Matchmaking.make_games_players(game, team_2, 2, team_2_expected_result)
 
     { team_1: team_1, team_2: team_2 }
   end
@@ -40,7 +15,7 @@ module Matchmaking
 
     pairs = []
 
-    pool.sort_by { |player| player.elo }
+    pool.sort { |a, b| a.elo <=> b.elo }
 
     until pool.length == 0
       pairs << [pool.shift, pool.pop]
@@ -53,9 +28,11 @@ module Matchmaking
     team_1 << pairs.pop
     team_2 << pairs.shift
     team_2 << pairs.pop
+
     team_1.flatten!
     team_2.flatten!
     pairs.flatten!
+    
     if Elo.elo_average(team_1) > Elo.elo_average(team_2)
       team_1 << pairs[0]
       team_2 << pairs[1]
@@ -67,4 +44,21 @@ module Matchmaking
     { team_1: team_1, team_2: team_2 }
   end
 
+
+  def self.make_games_players(game, team, team_number, expected_result)
+    team.map { |player|
+      expected_change = Elo.expected_change(player.elo, expected_result)
+
+      details = {
+        team: team_number,
+        player_id: player.id,
+        game_id: game.id,
+        pregame_elo: player.elo,
+        expected_loss_change: expected_change[:loss],
+        expected_win_change: expected_change[:win]
+      }
+      GamesPlayer.create(details)
+    }
+
+  end
 end
